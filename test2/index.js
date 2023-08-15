52a88cc64d48f5ca9761b19a6cb765d47cd98a5c
MD20M <78083321+MD20M@users.noreply.github.com>
2023-08-15 19:11:11 +0000

    html

--- test2/index.js
+++ test2/index.js
@@ -8,13 +8,26 @@ import {
 } from "https://unpkg.com/@web3modal/ethereum@2.6.2";
 import { Web3Modal } from "https://unpkg.com/@web3modal/html@2.6.2";
 
+//import { Connection, PublicKey } from 'https://unpkg.com/@solana/web3.js@latest/lib/index.iife.js';
+
+
+
 //solana wallet adapter for all solana wallets in javascript using https imports
 //import { WalletAdapterNetwork } from "https://cdn.jsdelivr.net/npm/@solana/wallet-adapter-base";
 
 
 
+//console.log(solanaWeb3);
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("devnet"));
+
+const keypairs = solanaWeb3.Keypair.generate();
+
+console.log(keypairs);
 
 
+
+//import { PhantomWallet } from "../node_modules/@solana/wallet-adapter-phantom";
+//console.log(solWeb3);
 // 0. Import wagmi dependencies
 const { mainnet, polygon, avalanche, arbitrum } = WagmiCoreChains;
 const { configureChains, createConfig } = WagmiCore;
@@ -22,6 +35,8 @@ const { configureChains, createConfig } = WagmiCore;
 // 1. Define chains
 const chains = [mainnet, polygon, avalanche, arbitrum];
 const projectId = "0205b72056f86f430adfbbf1a2a05180";
+
+
 
 // 2. Configure wagmi client
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
@@ -38,6 +53,22 @@ const wagmiConfig = createConfig({
   ],
   publicClient,
 });
+
+const ethClient = new EthereumClient(wagmiConfig, chains)
+const web3modal = new Web3Modal({ projectId }, ethClient)
+
+let connectButton = document.getElementById("connectButton");
+
+connectButton.addEventListener("click", async () => {
+  await web3modal.openModal()
+});
+
+let changeChainButton = document.getElementById("changeChainButton");
+
+changeChainButton.addEventListener("click", async () => {
+  await web3modal.setDefaultChain(polygon)
+});
+
 
 // 3. Create ethereum and modal clients
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
